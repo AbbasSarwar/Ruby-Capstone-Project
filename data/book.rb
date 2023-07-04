0c2f320c550a35c1d187a7c7b509cc77bd6aba56
Tayyab-Kamboh <tayyabkambohSK28@gmail.com>
2023-07-04 13:51:33 +0200

    fix linter issues

--- data/book.rb
+++ data/book.rb
@@ -1,15 +1,15 @@
-require_relative 'item' 
+require_relative 'item'
 
 class Book < Item
   attr_accessor :author, :cover_state
 
-  def initialize(genre, source, label, publish_date, archived, author, cover_state)
-    super(genre, source, label, publish_date, archived)
-    @author = author
-    @cover_state = cover_state
+  def initialize(options = {})
+    super(options[:genre], options[:source], options[:label], options[:publish_date], options[:archived])
+    @author = options[:author]
+    @cover_state = options[:cover_state]
   end
 
   def can_be_archived?
-    super || cover_state == "bad"
+    super || cover_state == 'bad'
   end
 end
