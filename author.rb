f1826d2bb45781dbdd2eceb3e25e6205127c1342
otegaa <ekiugbotega@gmail.com>
2023-07-08 08:21:04 +0200

    Create game and author class and initialized them

---
+++ author.rb
@@ -0,0 +1,12 @@
+class Author
+  attr_reader :id, :first_name, :last_name
+  attr_accessor :items
+
+  def initialize(id, first_name, last_name)
+    @id = id || Random.rand(1..1000)
+    @first_name = first_name
+    @last_name = last_name
+    @items = []
+  end
+  
+end
