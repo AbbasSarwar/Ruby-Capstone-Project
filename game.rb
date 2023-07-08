d35eff6f9e8fb83ccbdc287dbe5151e3ee977481
otegaa <ekiugbotega@gmail.com>
2023-07-08 09:32:39 +0200

    Implement overriding can_be_archived method from parent class in the Game class

--- game.rb
+++ game.rb
@@ -6,7 +6,13 @@ class Game < Item
   def initialize(id, publish_date, multiplayer, last_played_at)
     super(id, publish_date)
     @multiplayer = multiplayer
-    @last_played_at = last_played_at
+    @last_played_at = Date.parse(last_played_at)
   end
 
+  private
+
+  def can_be_archived?
+    parent_method = super
+    parent_method && @last_played_at < (Date.today - 365 * 2)
+  end
 end
